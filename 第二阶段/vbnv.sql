56763740555c46d4f5f28d86470dfbb6f3f1b4cd
hdqqkq <2113826067@qq.com>
2016-11-29 13:02:51 +0800

    第二阶段材料

---
+++ 第二阶段/vbnv.sql
@@ -0,0 +1,241 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: canting
+-- ------------------------------------------------------
+-- Server version	5.7.11-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admin` (
+  `adminid` int(11) NOT NULL,
+  `adminname` varchar(45) NOT NULL,
+  `admintype` varchar(45) NOT NULL,
+  `adminpsd` varchar(45) NOT NULL,
+  PRIMARY KEY (`adminid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `evaluate`
+--
+
+DROP TABLE IF EXISTS `evaluate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `evaluate` (
+  `evaluateid` int(11) NOT NULL,
+  `evaluateme` varchar(45) NOT NULL,
+  `evaluatepr` varchar(45) NOT NULL,
+  `evaluatecnu` varchar(45) NOT NULL,
+  PRIMARY KEY (`evaluateid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `evaluate`
+--
+
+LOCK TABLES `evaluate` WRITE;
+/*!40000 ALTER TABLE `evaluate` DISABLE KEYS */;
+/*!40000 ALTER TABLE `evaluate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `greens`
+--
+
+DROP TABLE IF EXISTS `greens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `greens` (
+  `greensid` int(11) NOT NULL AUTO_INCREMENT,
+  `greensname` varchar(45) NOT NULL,
+  `greensprice` varchar(45) NOT NULL,
+  `greensfov` varchar(45) NOT NULL,
+  `greensfocprice` varchar(45) NOT NULL,
+  `greensgrade` varchar(45) NOT NULL,
+  `greenspic` varchar(45) NOT NULL,
+  `greensdes` varchar(45) NOT NULL,
+  `greenstype` varchar(45) NOT NULL,
+  PRIMARY KEY (`greensid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `greens`
+--
+
+LOCK TABLES `greens` WRITE;
+/*!40000 ALTER TABLE `greens` DISABLE KEYS */;
+/*!40000 ALTER TABLE `greens` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jurisdiction`
+--
+
+DROP TABLE IF EXISTS `jurisdiction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jurisdiction` (
+  `jurisdictionid` int(11) NOT NULL,
+  `jurisdictiontype` varchar(45) NOT NULL,
+  `jurisdictioncol` varchar(45) NOT NULL,
+  PRIMARY KEY (`jurisdictionid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jurisdiction`
+--
+
+LOCK TABLES `jurisdiction` WRITE;
+/*!40000 ALTER TABLE `jurisdiction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jurisdiction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order1`
+--
+
+DROP TABLE IF EXISTS `order1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order1` (
+  `orderid` int(11) NOT NULL AUTO_INCREMENT,
+  `orderoid` varchar(45) NOT NULL,
+  `userid` varchar(45) NOT NULL,
+  `deal` varchar(45) NOT NULL,
+  `admin` varchar(45) NOT NULL,
+  `greensid` varchar(45) NOT NULL,
+  `greensq` varchar(45) NOT NULL,
+  `tablenumber` varchar(45) NOT NULL,
+  `usernumber` varchar(45) NOT NULL,
+  `ordertime` varchar(45) NOT NULL,
+  PRIMARY KEY (`orderid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order1`
+--
+
+LOCK TABLES `order1` WRITE;
+/*!40000 ALTER TABLE `order1` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rownumer`
+--
+
+DROP TABLE IF EXISTS `rownumer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rownumer` (
+  `rownumerid` int(11) NOT NULL,
+  `rownumeror` varchar(45) NOT NULL,
+  `rownumertime` varchar(45) NOT NULL,
+  PRIMARY KEY (`rownumerid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rownumer`
+--
+
+LOCK TABLES `rownumer` WRITE;
+/*!40000 ALTER TABLE `rownumer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rownumer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `table1`
+--
+
+DROP TABLE IF EXISTS `table1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `table1` (
+  `tableid` int(11) NOT NULL AUTO_INCREMENT,
+  `table1number` varchar(45) NOT NULL,
+  `zhuangtai` varchar(45) NOT NULL,
+  PRIMARY KEY (`tableid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `table1`
+--
+
+LOCK TABLES `table1` WRITE;
+/*!40000 ALTER TABLE `table1` DISABLE KEYS */;
+/*!40000 ALTER TABLE `table1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `jifen` int(11) NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'canting'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-11-28 17:43:56
